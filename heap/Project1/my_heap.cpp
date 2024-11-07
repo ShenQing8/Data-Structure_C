9169e63801e8459064adafe93d35d3c35bca090a
SQ <1653216274@qq.com>
2024-11-07 21:00:24 +0800

    test

--- heap/Project1/my_heap.cpp
+++ heap/Project1/my_heap.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
 // 堆是一种满足条件的完全二叉树，分为大顶堆和小顶堆
@@ -10,11 +11,9 @@ using namespace std;
 // 完全二叉树非常适合用数组来表示。由于堆正是一种完全二叉树，因此我们将采用数组来存储堆。
 
 
-class Heap
+class MaxHeap
 {
-public:
-	vector<int> myHeap;
-
+private:
 	/*获取左子节点*/
 	int left(int i)// 接收下标
 	{
@@ -30,17 +29,9 @@ public:
 	{
 		return (i - 1) / 2;// 向下取整
 	}
-
-	/*访问堆顶元素*/
-	int peek()
-	{
-		return myHeap[0];
-	}
-
 	/*自下而上调整*/
-	void siftUp()
+	void siftUp(int i)
 	{
-		int i = myHeap.size() - 1;
 		int p = parent(i);
 		while (p >= 0)
 		{
@@ -53,9 +44,8 @@ public:
 		}
 	}
 	/*自上而下调整*/
-	void siftDown()
+	void siftDown(int i)
 	{
-		int i = 0;
 		int l = left(i);
 		int r = right(i);
 		int sz = myHeap.size();
@@ -71,6 +61,23 @@ public:
 			r = right(i);
 		}
 	}
+public:
+	vector<int> myHeap;
+	
+	/*建堆*/
+	MaxHeap(vector<int>& input)
+	{
+		myHeap = input;
+		// 堆化除叶节点以外的其他节点
+		for (int i = parent(myHeap.size() - 1); i >= 0; --i)
+			siftDown(i);
+	}
+
+	/*访问堆顶元素*/
+	int peek()
+	{
+		return myHeap[0];
+	}
 
 	/*元素入堆*/
 	void push(int val)
@@ -78,7 +85,7 @@ public:
 		// 入堆
 		myHeap.push_back(val);
 		// 调整堆
-		siftUp();
+		siftUp(myHeap.size() - 1);
 	}
 	/*元素出堆*/
 	void pop()
@@ -90,12 +97,38 @@ public:
 		// 再删除最后一个元素
 		myHeap.pop_back();
 		// 自上而下调整
+		siftDown(0);
 	}
 };
 
+/*关于堆的 Top-K 问题*/
+priority_queue<int, vector<int>, greater<int>> topkHeap(vector<int>& nums,int k)
+{
+	priority_queue<int, vector<int>, greater<int>> ret;
+	// 初始化一个大小为 k 的小顶堆
+	for (int i = 0; i < k; ++i)
+		ret.push(nums[i]);
+	// 循环剩余 n-k 个元素，如果有大于堆顶元素的，替换掉
+	for (int i = nums.size() - 1; i >= k; --i)
+		if (nums[i] > ret.top())
+		{
+			ret.pop();
+			ret.push(nums[i]);
+		}
+	return ret;
+
+}
+void topK()
+{
+	// 给定一个长度为 n 的无序数组 nums ，请返回数组中最大的 k 个元素。
+	vector<int> nums = { 2,6,4,1,8,9,23,41,20,37,56 };
+	priority_queue<int, vector<int>, greater<int>> topk = topkHeap(nums, 3);
+}
 
 int main()
 {
-	
+	vector<int> nums = { 2,6,4,1,8,9,23,41,20,37,56 };
+	MaxHeap maxHeap(nums);
+	topK();
 	return 0;
 }
